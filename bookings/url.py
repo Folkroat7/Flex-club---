901510a2e92e51c653f48e0ed2defde178b8e810
Folkroat7 <xenfrolova17@gmail.com>
2026-05-13 16:20:53 +0300

    Remove BookerDestroyAPIView from urlpatterns

--- bookings/url.py
+++ bookings/url.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import ScedListAPIView, BookingCreateAPIView, BookerDestroyAPIViev, mainPage, schedPage, contactsPage, self
+from .views import ScedListAPIView, BookingCreateAPIView, mainPage, schedPage, contactsPage, self
 
 urlpatterns = [
     path('api/sched/', ScedListAPIView.as_view(), name= 'sched-list'),
     path('api/book/', BookingCreateAPIView.as_view(), name = 'booking-create'),
-    path('api/book/<int:pk>/', BookerDestroyAPIViev.as_view(), name = 'booker-delete'),
     path('', mainPage, name='new-page'),
     path('schedule/', schedPage, name = 'sched-page'),
     path('contacts/', contactsPage, name= 'contacts-page'),
